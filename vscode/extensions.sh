607e5bc1b5f5a15f54ed6af2c3870d9f9a321958
Joel Bowen <hello@joel.software>
2019-11-30 20:37:35 -0500

    Improve VS Code extensions script

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,5 +1,18 @@
 . "./utils/helpers.sh"
 
-print_in_purple "\nInstalling VS Code extensions\n\n"
+declare -a VSCODE_EXTENSIONS=(
+    vscode-icons-team.vscode-icons # File Icons
+)
 
-code --install-extension vscode-icons-team.vscode-icons
+main() {
+    local i=""
+    print_in_purple "\nInstalling VS Code extensions\n\n"
+
+    for i in "${VSCODE_EXTENSIONS[@]}"; do
+
+        execute "code --install-extension $i"
+
+    done
+} 
+
+main
