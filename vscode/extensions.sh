6dd1634a2ed6eb1460c7a5c8c6be1e0d5cab45a9
Joel Bowen <hello@joel.software>
2019-11-30 20:38:00 -0500

    add vs code extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,7 +1,15 @@
 . "./utils/helpers.sh"
 
 declare -a VSCODE_EXTENSIONS=(
+    dbaeumer.vscode-eslint # Eslint
+    esbenp.prettier-vscode # Prettier
+    ms-python.python # Pyhon
+    ms-vsliveshare.vsliveshare # Live Share
+    octref.vetur # Vue Tools
+    prisma.vscode-graphql # GraphQL Tooling
+    tombonnike.vscode-status-bar-format-toggle # Disable auto-formatting
     vscode-icons-team.vscode-icons # File Icons
+    wix.vscode-import-cost # Print import file sizes
 )
 
 main() {
