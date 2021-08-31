fe3f6eef2ab761b07673f85b9a94ccaf197c6138
Joel Bowen <hello@joel.software>
2021-08-31 09:46:32 -0600

    feat: prettierrc for this repo

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -2,7 +2,7 @@
 . "./utils/helpers.sh"
 
 # finds all .dotfiles in this folder
-FIND_OPTIONS="-maxdepth 1 -name \".*\" -not -name .DS_Store -not -name .git -not -name . | sed -e 's|//|/|' | sed -e 's|./.|.|'"
+FIND_OPTIONS="-maxdepth 1 -name \".*\" -not -name .DS_Store -not -name .git -not -name .prettierrc -not -name . | sed -e 's|//|/|' | sed -e 's|./.|.|'"
 declare -a FILES_TO_SYMLINK=$(eval "find . -type d $FIND_OPTIONS")
 declare -a FOLDERS_TO_SYMLINK=$(eval "find . -type f $FIND_OPTIONS")
 declare -a ITEMS_TO_SYMLINK=($FILES_TO_SYMLINK $FOLDERS_TO_SYMLINK)
@@ -51,7 +51,7 @@ main() {
     # oh my zsh custom plugins and themes
     symlink "$(pwd)/oh-my-zsh/plugins" "$HOME/.oh-my-zsh/custom/plugins" false
     symlink "$(pwd)/oh-my-zsh/themes" "$HOME/.oh-my-zsh/custom/themes" false
-    
+
     # vscode
     symlink "$(pwd)/vscode/settings.json" "$HOME/Library/Application\ Support/Code/User/settings.json" false
 }
