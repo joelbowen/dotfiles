d7e597670687ac6078b3d6061e2412e161e4d09e
Joel Bowen <1557848+joelbowen@users.noreply.github.com>
2022-07-15 20:54:03 -0600

    chore(code): update extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,6 +1,7 @@
 . "./utils/helpers.sh"
 
 declare -a VSCODE_EXTENSIONS=(
+    amazonwebservices.aws-toolkit-vscode
     byCedric.vscode-expo
     castwide.solargraph
     christian-kohler.npm-intellisense
@@ -9,13 +10,16 @@ declare -a VSCODE_EXTENSIONS=(
     EditorConfig.EditorConfig
     esbenp.prettier-vscode
     GitHub.codespaces
+    GitHub.copilot
     golang.go
     GraphQL.vscode-graphql
+    hashicorp.terraform
     hbenl.vscode-test-explorer
     herrmannplatz.npm-dependency-links
     jemmyw.rails-fast-nav
     jnbt.vscode-rufo
     JuanBlanco.solidity
+    kddejong.vscode-cfn-lint
     misogi.ruby-rubocop
     ms-azuretools.vscode-docker
     ms-python.python
@@ -23,15 +27,21 @@ declare -a VSCODE_EXTENSIONS=(
     ms-toolsai.jupyter
     ms-toolsai.jupyter-keymap
     ms-toolsai.jupyter-renderers
+    ms-vscode-remote.remote-containers
     ms-vscode.test-adapter-converter
     ms-vsliveshare.vsliveshare
     octref.vetur
     Prisma.prisma
+    rangav.vscode-thunder-client
     rebornix.ruby
+    redhat.vscode-yaml
     streetsidesoftware.code-spell-checker
+    sysoev.vscode-open-in-github
+    ThreadHeap.serverless-ide-vscode
     tombonnike.vscode-status-bar-format-toggle
     vscode-icons-team.vscode-icons
     vscodevim.vim
+    wayou.vscode-todo-highlight
     wingrunr21.vscode-ruby
     wix.vscode-import-cost
 )
